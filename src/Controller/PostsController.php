55b19316e9d67c1fddd106db9c0290d780a3d349
arcanoid <info@fedushin.ru>
2024-06-06 21:59:13 +0400

    SELECT list & one record

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,28 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PostsController extends AbstractController
 {
+    /** @var PostRepository $postRepository */
+    private $postRepository;
+
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
     #[Route('/posts', name: 'app_posts')]
     public function index(): Response
     {
-        $posts = [
-            'post_1' => [
-                'title' => 'Заголовок первого поста',
-                'body' => 'Тело первого поста'
-            ],
-            'post_2' => [
-                'title' => 'Заголовок второго поста',
-                'body' => 'Тело второго поста'
-            ]
-        ];
+        $posts = $this->postRepository->findAll();
 
         return $this->render('posts/index.html.twig', [
-            'posts' => $posts,
+            'posts' => $posts
+        ]);
+    }
+
+    #[Route("/posts/{id}", name: "blog_show")]
+    public function post(Post $post)
+    {
+        return $this->render('posts/show.html.twig', [
+            'post' => $post
         ]);
     }
 }
