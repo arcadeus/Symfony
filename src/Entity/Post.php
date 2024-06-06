de1e878b79a49158bcf5aa2ae449e6173190493d
arcanoid <info@fedushin.ru>
2024-06-06 19:47:59 +0400

    Tune post Entity $ php bin/console make:entity Post ... $ php bin/console make:migration $ php bin/console doctrine:migrations:migrate

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -13,8 +13,38 @@ class Post
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $body = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): static
+    {
+        $this->body = $body;
+
+        return $this;
     }
 }
