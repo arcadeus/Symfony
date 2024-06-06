7bc8daa7827085cf210e5961c7481a728bf648e9
arcanoid <info@fedushin.ru>
2024-06-06 18:49:46 +0400

    Customize http://127.0.0.1:8000/posts

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -11,8 +11,19 @@ class PostsController extends AbstractController
     #[Route('/posts', name: 'app_posts')]
     public function index(): Response
     {
+        $posts = [
+            'post_1' => [
+                'title' => 'Заголовок первого поста',
+                'body' => 'Тело первого поста'
+            ],
+            'post_2' => [
+                'title' => 'Заголовок второго поста',
+                'body' => 'Тело второго поста'
+            ]
+        ];
+
         return $this->render('posts/index.html.twig', [
-            'controller_name' => 'PostsController',
+            'posts' => $posts,
         ]);
     }
 }
