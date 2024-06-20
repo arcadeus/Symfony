de0621fb84889b50ba84d148c46ca2821f22bcf9
arcanoid <info@fedushin.ru>
2024-06-20 21:47:19 +0400

    Add OrderTest::testFields()

--- tests/Controller/OrderTest.php
+++ tests/Controller/OrderTest.php
@@ -3,9 +3,19 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Security\Core\User\InMemoryUser;
 
 class OrderTest extends WebTestCase
 {
+    //
+    private function login()
+    {
+        $client = static::createClient();
+        $user = new InMemoryUser('user', '123', ['ROLE_USER']);
+        $client->loginUser($user);
+        return $client;
+    }
+
     //
     // При открытии формы заказа неавторизованным пользователем отображается страница с ошибкой
     //
@@ -16,5 +26,40 @@ class OrderTest extends WebTestCase
 
         $this->assertResponseStatusCodeSame(401, 'Страница заказа должна быть доступна только авторизованным пользователям');
         $this->assertSelectorTextContains('h1', '401', 'Заголовок сообщения об ошибке доступа должен содержать "401"');
+    }
+
+    //
+    // Авторизованному пользователю отображается форма, проверить, что вывелись все поля и кнопка
+    //
+    public function testFields(): void
+    {
+        $client = $this->login();
+
+        $crawler = $client->request('GET', '/order');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Заказать услугу оценки');
+
+        $this->assertCount(
+            1,
+            $crawler->filter('select#order_service'),
+            'Должно быть ровно одно поле "Услуга"'
+        );
+        $this->assertCount(
+            1,
+            $crawler->filter('input[type=email]#order_email'),
+            'Должно быть ровно одно поле "Электонная почта"'
+        );
+        $this->assertCount(
+            1,
+            $crawler->filter('input[type=text][readonly=readonly]#price'),
+            'Должно быть ровно одно поле "Стоимость" (только-чтение)'
+        );
+
+        $this->assertCount(
+            1,
+            $crawler->filter('input[type=submit]'),
+            'Должно быть ровно одна кнопка "Подтвердить"'
+        );
     }
 }
