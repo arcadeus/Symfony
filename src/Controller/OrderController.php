fd9ea69f68488417424c5a6d57c37d7e60ac5e00
arcanoid <info@fedushin.ru>
2024-06-20 11:11:18 +0400

    Tune Order form

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,17 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Form\OrderType;
+use App\Repository\ServiceRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class OrderController extends AbstractController
 {
-    #[Route('/order', name: 'app_order')]
-    public function index(): Response
+    private ManagerRegistry $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
     {
-        return $this->render('order/index.html.twig', [
-            'controller_name' => 'OrderController',
+        $this->doctrine = $doctrine;
+    }
+
+    #[Route('/order', name: 'app_order')]
+    public function add(Request $request, ServiceRepository $serviceRepository): Response
+    {
+        $new_order = new Order();
+        $form = $this->createForm(
+            OrderType::class,
+            $new_order,
+            ['services' => $serviceRepository->findAll()]
+        );
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->doctrine->getManager();
+            $em->persist($new_order);
+            $em->flush();
+
+            return $this->redirectToRoute('app_order');
+        }
+
+        return $this->render('order/add.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -6,6 +6,8 @@ use App\Entity\Order;
 use App\Entity\Service;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,18 +16,26 @@ class OrderType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('service', EntityType::class, [
-                'class' => Service::class,
-                'choice_label' => 'id',
-            ])
-        ;
+            ->add('email', EmailType::class, [
+                'label' => ' '
+              ])
+            ->add('service', ChoiceType::class, [
+                'label' => ' ',
+                'choices' => $options['services'],
+                'choice_value' => 'id',
+                'choice_label' => 'name',
+                'choice_attr' => function ($service): array {
+                    return ['data-price' => $service->getPrice()];
+                },
+                'attr' => ['onchange' => 'change_service()'],
+              ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Order::class,
+            'services' => null
         ]);
     }
 }
